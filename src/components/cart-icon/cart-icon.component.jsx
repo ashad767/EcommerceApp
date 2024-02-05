99305fa13777243af79c2c0602b2a56cad819fda
ashad767 <ashadahmed247@gmail.com>
2024-02-04 19:27:35 -0500

    product card, cart dropdown, and cart icon components

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,16 @@
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
+
+import './cart-icon.styles.scss'
+
+import React from 'react'
+
+const CartIcon = () => {
+  return (
+    <div className='cart-icon-container'>
+        <ShoppingIcon className='shopping-icon'/>
+        <div className='item-count'>0</div>
+    </div>
+  )
+}
+
+export default CartIcon
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,9 +2,12 @@ import { Fragment, useContext } from 'react'
 import { Link, Outlet } from 'react-router-dom'
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
+import CartIcon from '../../components/cart-icon/cart-icon.component'
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
+
 import { UserContext } from '../../contexts/user.context'
 
-import { signOutUser } from '../../utils/firebase/firebase.utils' 
+import { signOutUser } from '../../utils/firebase/firebase.utils'
 
 import './navigation.styles.scss'
 
@@ -21,20 +24,22 @@ const Navigation = () => {
                     <Link className="nav-link" to="/shop">
                         SHOP
                     </Link>
-                    {currentUser ? 
+                    {currentUser ?
                         <span className="nav-link" onClick={signOutUser}>
                             SIGN OUT
-                        </span>  
-                        :  
+                        </span>
+                        :
                         <Link className="nav-link" to="/auth">
                             SIGN IN
                         </Link>
                     }
+                    <CartIcon />
                 </div>
+                <CartDropdown />
             </div>
             <Outlet />
         </Fragment>
     )
-}  
+}
 
 export default Navigation
