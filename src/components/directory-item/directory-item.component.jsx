a1410734c0365d68b11703aef196ab1a55b55007
ashad767 <ashadahmed247@gmail.com>
2024-05-16 02:36:58 -0400

    added links to directory categories

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,13 +1,20 @@
+import { useNavigate } from 'react-router-dom';
 import './directory-item.styles.scss'
 
 const DirectoryItem = ({ category }) => {
     const { title, imageUrl } = category;
+    const nav = useNavigate();
+
+    const redirect = () => {
+        nav(`/shop/${title}`);
+    }
 
     return (
-        <div className="directory-item-container">
+        <div className="directory-item-container" onClick={redirect}>
             <div className="background-image" style={{
                 backgroundImage: `url(${imageUrl})`
-            }} />
+            }}>
+            </div>
             <div className="body">
                 <h2>{title}</h2>
                 <p>Shop Now</p>
