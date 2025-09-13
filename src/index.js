b6cb63c2a5aa14115c2ededc0476a0a57802d41e
Ashad Ahmed <ashadahmed247@gmail.com>
2025-09-12 22:11:55 -0400

    start adding stripe

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import App from './App';
 import { UserProvider } from './contexts/user.context';
 import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
+import { Elements } from '@stripe/react-stripe-js';
+import { stripePromise } from './utils/stripe/stripe.utils';
 
 import './index.scss';
 
@@ -16,7 +18,9 @@ root.render(
       <UserProvider>
         <CategoriesProvider>
           <CartProvider>
-            <App />
+            <Elements stripe={stripePromise}>
+              <App />
+            </Elements>
           </CartProvider>
         </CategoriesProvider>
       </UserProvider>
